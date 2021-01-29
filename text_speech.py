d3fc0465eb521ef0a49a80561a3e659db851a706
Harsh Kulkarni <72391096+harshkulkarni17@users.noreply.github.com>
2021-01-29 22:22:47 +0530

    Update text_speech.py

--- text_speech.py
+++ text_speech.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 def textToSpeech():
-    from gtts import gTTS
-    import os
+    import pyttsx3
 
     # The text that you want to convert to audio
     f1 = open("static/speech.txt", "r")
@@ -9,45 +7,6 @@ def textToSpeech():
     f1.close()
     mytext = output
 
-    # Language in which you want to convert
-    language = 'en'
-
-    # Passing the text and language to the engine,
-    # here we have marked slow=False. Which tells
-    # the module that the converted audio should
-    # have a high speed
-    myobj = gTTS(text=mytext, lang=language, slow=False)
-
-    # Saving the converted audio in a mp3 file named
-    # welcome
-    myobj.save("welcome.mp3")
-
-    # Playing the converted file
-    return (os.system("welcome.mp3"))
-=======
-def textToSpeech():
-    from gtts import gTTS
-    import os
-
-    # The text that you want to convert to audio
-    f1 = open("static/speech.txt", "r")
-    output = f1.readline()
-    f1.close()
-    mytext = output
-
-    # Language in which you want to convert
-    language = 'en'
-
-    # Passing the text and language to the engine,
-    # here we have marked slow=False. Which tells
-    # the module that the converted audio should
-    # have a high speed
-    myobj = gTTS(text=mytext, lang=language, slow=False)
-
-    # Saving the converted audio in a mp3 file named
-    # welcome
-    myobj.save("welcome.mp3")
-
-    # Playing the converted file
-    return (os.system("welcome.mp3"))
->>>>>>> b79508fbaa225bb2143e65caeefe5f5cdf449d6d
+    engine = pyttsx3.init()
+    engine.say(mytext)
+    return engine.runAndWait()
